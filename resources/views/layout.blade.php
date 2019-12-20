0d332316dfe584c090dae3c45dbd55e13f764b78
Elijah Prince <elijahprince73@gmail.com>
2019-12-19 22:45:52 -0800

    Creating new routes and using blade templating engine to recieve data and display it

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>@yield('title')</title>
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+  <!-- Styles -->
+  <style>
+    html,
+    body {
+      background-color: #fff;
+      color: #636b6f;
+      font-family: 'Nunito', sans-serif;
+      font-weight: 200;
+    }
+
+    .links>a {
+      color: #636b6f;
+      padding: 0 25px;
+      font-size: 13px;
+      font-weight: 600;
+      letter-spacing: .1rem;
+      text-decoration: none;
+      text-transform: uppercase;
+    }
+  </style>
+</head>
+
+<body>
+  <div class="links">
+    <a href="/">Home</a>
+    <a href="/contact">Contact</a>
+    <a href="/about">About</a>
+  </div>
+
+  @yield('content')
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,19 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    $task = [
+        'Go to the store',
+        'Go to the market',
+        'Go to the work',
+    ];
+
+    return view('welcome', [ 'tasks' => $task]);
+});
+
+Route::get('/about', function() {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
